2100b3261e1bdf4be1d00680206ab1c7451f8239
Rajavenkateshwaran Subramanian <rajavn6789@gmail.com>
2019-08-11 21:16:10 +0530

    Update rpm_rev_lights.ino

--- adruino/rpm_rev_lights.ino
+++ adruino/rpm_rev_lights.ino
@@ -52,8 +52,7 @@ DEFINE_GRADIENT_PALETTE( rpm_palatte) {
     
     if (Serial.available() > 0) {
       incoming = Serial.readString();
-      int posColon = incoming.indexOf(':');
-      String ledString = incoming.substring(0, posColon);
+      String ledString = getValue(incoming, ':', 0);
       int numLeds = ledString.toInt();
 
       previousMillis = currentMillis; // reset
@@ -115,3 +114,20 @@ DEFINE_GRADIENT_PALETTE( rpm_palatte) {
     fill_solid(leds, NUM_LEDS, CRGB::Black);
     FastLED.show();
   }
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i = 0; i <= maxIndex && found <= index; i++){
+    if(data.charAt(i) == separator || i == maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
